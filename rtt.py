151249e27b9a8f79537f9b9d174839889848a2f9
Alex Simoneau <afsimoneau@gmail.com>
2020-09-16 16:16:18 -0400

    edits

--- rtt.py
+++ rtt.py
@@ -1,19 +1,21 @@
-import os,sys,subprocess
+import os,sys,subprocess,re
 
 lines = []
 with os.popen("tshark -r "+sys.argv[1]+" -T fields -e tcp.analysis.ack_rtt") as sharky:
     for line in sharky:
         lines.append(line)
 
-lines = list(filter(None,lines))#remove whitespace
+cleanList = []
+for dirty in lines:
+    temp = re.sub("[^\d.]","",dirty)
+    if temp!="":
+        cleanList.append(temp)#filtering
 numbers = list(map(float,lines))#convert to float
 
 print(sum(numbers)/len(numbers))#print result. can use numbers list to produce a plot though
 
 '''
-run this script to get average RTT
+run this script to get RTT
 
 tshark -r <.pcap file name> -T fields -e tcp.analysis.ack_rtt
-
-note: tshark is doing no filtering on non-ack packets so there is whitespace (hence the filter)
 '''
