b03bda8d7fcc0b5f6f3f2cd424ddc04733b328f6
Alex Simoneau <afsimoneau@gmail.com>
2020-09-16 16:05:02 -0400

    edits

--- README.md
+++ README.md
@@ -2,3 +2,16 @@
 Test suite and scripts for WPI students working on the ISU "CCA over a satellite"
 
 Useage notes: This is to simplify the distribution, so mainly using master is fine unless you want to edit/improve a script that is already established
+
+Tools to use:
+
+UDPing
+iperf3
+tcpdump 
+    use -s for snaplen
+    use -w for write (need file name)
+    use -i for interface (eno2)
+    
+screen (to pop out tcpdump into its own terminal)
+tshark
+sysctl
--- rtt.py
+++ rtt.py
@@ -1,17 +1,19 @@
-import sys,subprocess
+import os,sys,subprocess
 
 lines = []
-for line in sys.stdin:
-    lines.append(line)
+with os.popen("tshark -r "+sys.argv[1]+" -T fields -e tcp.analysis.ack_rtt") as sharky:
+    for line in sharky:
+        lines.append(line)
+
 lines = list(filter(None,lines))#remove whitespace
 numbers = list(map(float,lines))#convert to float
 
 print(sum(numbers)/len(numbers))#print result. can use numbers list to produce a plot though
 
 '''
-run this command to get average RTT
+run this script to get average RTT
 
-tshark -r <.pcap file name> -T fields -e tcp.analysis.ack_rtt > rtt.py
+tshark -r <.pcap file name> -T fields -e tcp.analysis.ack_rtt
 
 note: tshark is doing no filtering on non-ack packets so there is whitespace (hence the filter)
 '''
